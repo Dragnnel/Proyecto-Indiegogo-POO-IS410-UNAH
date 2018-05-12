94693445b8543da60728a4384e76b05af4416579
Lfe_Dragnnel <lfe9621@gmail.com>
2018-05-12 13:37:40 -0600

    actualizar

--- page/verificar-conexion.php
+++ page/verificar-conexion.php
@@ -5,19 +5,6 @@
 	$conexion = new Conexion();
 	echo "Conexion establecida <br>";
 
-	/*
-		SELECT codigo_usuario, codigo_genero, nombre, apellido, email, password, url_image_perfil FROM tbl_usuarios WHERE 1
-
-<<<<<<< HEAD
-
-
-		"SELECT codigo_usuario, codigo_genero, nombre, apellido, ".
-=======
-	$sql = 	"SELECT codigo_usuario, nombre, apellido, ".
->>>>>>> 8f678ea8816cbc44b81e47d60f2b4ab5bcb171be
-			"email, password, url_image_perfil ".
-			"FROM tbl_usuarios"
-	*/
 
 	$sql = 	"SELECT codigo_categoria, nombre_categoria FROM tbl_categorias";
 	
