69c09b53f0b0d95d6d32e8aa9a5b516118fb5935
Lfe_Dragnnel <lfe9621@gmail.com>
2018-05-11 23:44:31 -0600

    actualizar

--- ajax/obtener-post.php
+++ ajax/obtener-post.php
@@ -6,13 +6,13 @@
 	$sql = "SELECT a.codigo_post, b.nombre, b.apellido, a.nombre_campana, a.descripcion, a.fecha_de_publicacion, a.fecha_de_expiracion, a.url_image, a.monto_a_recaudar from tbl_post a INNER JOIN tbl_usuarios b on (a.codigo_usuario = b.codigo_usuario)";
 
     $resultado = $conexion->ejecutarConsulta($sql);
-    $resultadoPost = array();
+    $respuesta = array();
     while($fila = $conexion->obtenerFila($resultado)){
-        $resultadoPost[] = $fila;
+        $respuesta[] = $fila;
     }
-
-
- 	echo json_encode($respuesta);
+    
+    echo json_encode($respuesta);
+    var_dump($respuesta);
     $conexion->cerrarConexion();
 	
 ?>
